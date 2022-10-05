d3bd81b04ea0b97c8767648f0d1d9c54b46044a1
Kevin He <72520741+thekevinlab@users.noreply.github.com>
2022-10-05 18:12:34 -0400

    fix: offset cursor x and y to make a better representation of actual cursor position

--- src/scripts/cursor.js
+++ src/scripts/cursor.js
@@ -2,7 +2,7 @@ $(document).on('mousemove', function (e) {
     const mouseY = e.clientY;
     const mouseX = e.clientX;
 
-    $('#cursor').css('transform', `translate3d(${mouseX}px, ${mouseY}px, 0)`);
+    $('#cursor').css('transform', `translate3d(${mouseX - 10}px, ${mouseY - 10}px, 0)`);
 });
 
 $(window).on('focus', function () {
