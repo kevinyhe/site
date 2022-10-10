686fd70ec9fcf2740fcaad1e03ad64e4cf34050a
Kevin He <72520741+thekevinlab@users.noreply.github.com>
2022-10-10 13:22:07 -0400

    style: fix text wrap

--- src/scripts/cursor.js
+++ src/scripts/cursor.js
@@ -8,12 +8,18 @@ $(document).on('mousemove', function (e) {
     $('#cursor').css('transform', `translate3d(${mouseX - 10}px, ${mouseY - 10}px, 0)`);
 });
 
-$(window).on('focus', function () {
-    $('#cursor').css('opacity', `1`);
-    $('#appMount').css('filter', `blur(0px)`);
-})
-
-$(window).on('blur', function () {
+function blurWindow () {
     $('#cursor').css('opacity', `0`);
     $('#appMount').css('filter', `blur(4px)`);
-})
+}
+
+function unBlurWindow () {
+    $('#cursor').css('opacity', `1`);
+    $('#appMount').css('filter', `blur(0px)`);
+}
+
+$(window).on('focus', unBlurWindow);
+$(window).on('blur', blurWindow);
+
+$(document).mouseleave(blurWindow);
+$(document).mouseenter(unBlurWindow);
