43d38f88466780cb6ce927acd1ba4a030c3daf3a
Kevin <72520741+kevinyhe@users.noreply.github.com>
2025-04-20 15:59:16 -0400

    remove old config routing

--- vite.config.js
+++ vite.config.js
@@ -31,7 +31,6 @@ export default defineConfig({
 				main: resolve(root, "index.html"),
 				contact: resolve(root, "contact/index.html"),
 				projects: resolve(root, "about/index.html"),
-				manewer: resolve(root, "manewer/index.html"),
 			},
 		},
 	},
